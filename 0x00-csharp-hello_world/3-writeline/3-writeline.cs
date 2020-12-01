2af9c5864a3b0a2c9a5d156eea236260dea9a3b1
mfcrespo <mafe_crespo@hotmail.com>
2020-11-30 20:11:43 -0500

    Write a C# script that prints exactly the thext

---
+++ 0x00-csharp-hello_world/3-writeline/3-writeline.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace _3_new_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programming is like building a multilingual puzzle");
+        }
+    }
+}
