83f13b4abf7c29357c797046e4c36a538e091016
mfcrespo <mafe_crespo@hotmail.com>
2020-12-01 01:22:27 -0500

    prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19 followed by a new line

--- 0x00-csharp-hello_world/100-hello_error/100-hello_error.cs
+++ 0x00-csharp-hello_world/100-hello_error/100-hello_error.cs
@@ -4,7 +4,7 @@ class Program
 {
 	static void Main(string[] args)
 	{
-		Console.WriteLine("and that piece of art is useful - Dora Korpar, 2015-10-19");
+		Console.Error.WriteLine("and that piece of art is useful - Dora Korpar, 2015-10-19");
 		Environment.Exit(1);
         }
 }
