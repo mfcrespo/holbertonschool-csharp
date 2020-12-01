06b6865b7dc8718ef6e9175799fc2156d68fe409
mfcrespo <mafe_crespo@hotmail.com>
2020-12-01 01:05:03 -0500

    Add task 1

--- 0x00-csharp-hello_world/1-build_new_project.sh
+++ 0x00-csharp-hello_world/1-build_new_project.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
-# Script that initializes and builds a new C# project inside a folder titled 1-new_project
-
-# Create folder and enter it
-mkdir -p 1-new_project; cd 1-new_project
+mkdir 0-new_project
+cd 0-new_project
 dotnet new console
-sleep 3
 dotnet build
-cd ..
