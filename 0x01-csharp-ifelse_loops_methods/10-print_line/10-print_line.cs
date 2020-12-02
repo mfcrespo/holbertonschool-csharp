958f8c3132b75cda6e14a51cd11366444dfadab3
mfcrespo <mafe_crespo@hotmail.com>
2020-12-02 00:00:16 -0500

    Write a method that draws a straight line in the terminal

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        if (length > 0)
+	{
+		Console.Write(new String('_', length));
+	}
+	Console.WriteLine();
+    }
+}
