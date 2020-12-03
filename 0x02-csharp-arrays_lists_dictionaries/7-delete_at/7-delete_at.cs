39a0674da57e3380eaaf4803440dd567b827f90d
mfcrespo <mafe_crespo@hotmail.com>
2020-12-02 23:58:24 -0500

    Write a method that deletes the item at a specific position in a list

---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        if (index > myList.Count - 1 || index < 0)
+        {
+            Console.WriteLine("Index out of range");
+            return (myList);
+        }
+	
+        for (int i = 0; i < myList.Count; i++)
+        {
+            if (i == index)
+            {
+                myList.Remove(myList[i]);
+                break;
+            }
+        }
+        return (myList);
+    }
+}
