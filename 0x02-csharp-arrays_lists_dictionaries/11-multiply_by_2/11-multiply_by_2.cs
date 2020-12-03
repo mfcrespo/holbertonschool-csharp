00aa47d440fde028a6c1cb4c96a2c98e8ba95c62
mfcrespo <mafe_crespo@hotmail.com>
2020-12-03 00:42:26 -0500

    Write a method that returns a new dictionary with all values multiplied by 2

---
+++ 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+    {
+        Dictionary<string, int> newDict = new Dictionary<string, int>();
+
+        if (myDict != null)
+        {
+            foreach (KeyValuePair<string, int> item in myDict)
+            {
+                newDict.Add(item.Key, item.Value * 2);
+            }
+        }
+        return (newDict);
+    }
+}
