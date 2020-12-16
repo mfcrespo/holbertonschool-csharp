1d34cc8920da801e7090575e3a965ba7e777b8f8
mfcrespo <mafe_crespo@hotmail.com>
2020-12-15 19:40:27 -0500

    Based on 0-enemy, write a public class Zombie

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,15 @@
+namespace Enemies
+{
+    /// <summary>Create an empty public class Zombie within Enemies that defines a zombie</summary>
+    public class Zombie
+    {
+        /// <summary>Int that represents the value of the Zombie health</summary>
+        public int health;
+
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/>class.</summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+    }
+}
