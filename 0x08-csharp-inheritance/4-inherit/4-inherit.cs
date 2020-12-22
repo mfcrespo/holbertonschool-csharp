48eef38b8196f1408a5d2aec4569ae9f19a5eeff
mfcrespo <mafe_crespo@hotmail.com>
2020-12-21 22:44:09 -0500

    a empty class Dog that inherits from empty class Animal

---
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,9 @@
+using System;
+
+/// <summary>Empty class Animal</summary>
+class Animal
+{}
+
+/// <summary>Write a empty class Dog that inherits from empty class Animal</summary>
+class Dog : Animal
+{}
