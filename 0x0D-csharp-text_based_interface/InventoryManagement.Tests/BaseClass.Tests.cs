5f60c4fe3384136fde2684ee0a81202b5863c3d1
Maria Crespo <mafe_crespo@hotmail.com>
2021-10-15 11:27:59 -0500

    unit test of BaseClass

--- 0x0D-csharp-text_based_interface/InventoryManagement.Tests/BaseClass.Tests.cs
+++ 0x0D-csharp-text_based_interface/InventoryManagement.Tests/BaseClass.Tests.cs
@@ -1,8 +1,9 @@
 using NUnit.Framework;
+using System;
 
-namespace InventoryManagement.Tests
+namespace InventoryLibrary.Tests
 {
-    public class Tests
+    public class BaseClass_Tests
     {
         [SetUp]
         public void Setup()
@@ -10,9 +11,31 @@ namespace InventoryManagement.Tests
         }
 
         [Test]
-        public void Test1()
+        public void BaseClass_Instance()
         {
-            Assert.Pass();
+            BaseClass bs = new BaseClass();
+            Assert.AreEqual(bs.date_created, bs.date_updated);
+        }
+
+        [Test]
+        public void BaseClass_Instance_Properties(
+            [Values("id", "date_created", "date_updated")] string prop)
+        {
+            BaseClass bs = new BaseClass();
+            Type myType = bs.GetType();
+
+            Assert.IsNotNull(myType.GetProperty(prop));
+        }
+
+        [Test]
+        public void BaseClass_Instance_Properties_Types()
+        {
+            BaseClass bs = new BaseClass();
+            Type myType = bs.GetType();
+
+            Assert.IsTrue(myType.GetProperty("id").PropertyType == typeof(string));
+            Assert.IsTrue(myType.GetProperty("date_created").PropertyType == typeof(DateTime));
+            Assert.IsTrue(myType.GetProperty("date_updated").PropertyType == typeof(DateTime));
         }
     }
 }
